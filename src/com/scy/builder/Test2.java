6dd438b04f8263ed67784adb79cf5d92072ab186
suocaiyuan <suocaiyuan@163.com>
2021-09-14 17:50:34 +0800

    建造者模式

---
+++ src/com/scy/builder/Test2.java
@@ -0,0 +1,27 @@
+package com.scy.builder;
+
+/**
+ * 类名： Test2 <br>
+ * 描述：TODO <br>
+ * 创建日期： 2021/9/14 <br>
+ *
+ * @author suocaiyuan
+ * @version V1.0
+ */
+public class Test2 {
+
+    public static void main(String[] args) {
+        Course2 course = new Course2.CourseBuilder()
+                .buildCourseName("Java")
+                .buildCourseArticle("article")
+                .buildCoursePPT("PPT")
+                .buildCourseVideo("Video")
+                .buildCourseQA("QA").build();
+        System.out.println(course);
+
+    //    Guava:
+     //   Set<String> set = ImmutableSet.<String>builder().add("a").build();
+
+        //BeanDefinitionBuilder
+    }
+}
